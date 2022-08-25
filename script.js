4d9ae348a4bafc06e8bf0a282da99861da426bd2
Stains <stainsmault@gmail.com>
2022-08-25 18:07:53 +0530

    calculator app

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
+
+
+// clicked object is 0 or not 
+
 function answer(e){
     let result = document.getElementById("result");
     let obj = e.target;
-    if (result==0){
+    if (result.value == 0){
         result.value = obj.textContent;
     }
     else{
-       result.value = result.value + obj.textContent; 
+        result.value = result.value + obj.textContent;
     }
-
+    
 }
 
-//signs 
+function back() {
+    let result = document.getElementById("result").value;
+    document.getElementById("result").value = result.substr(0, result.length - 1);
+}
+
+//signs
 let result = document.getElementById("result");
 let equal = document.getElementById("equal");
 let reset = document.getElementById("reset");
+let del = document.getElementById("delete");
+//sign and number execution using loop on click
+
 let symbols = document.getElementsByClassName("symbol");
 
-for (let i=0;i<symbols.lenght;i++){
+for (let i=0;i<symbols.length;i++){
     symbols[i].addEventListener("click",answer);
 }
 
@@ -28,4 +40,6 @@ equal.addEventListener("click", () => result.value = eval(result.value));
 // to clear eveyrthing 
 reset.addEventListener("click", ()=> document.getElementById("result").value = 0);
 
+//last text deletion 
 
+del.addEventListener("click",back);
